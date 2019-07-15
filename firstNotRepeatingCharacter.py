740a3b555c15c41e75044b0247bb221c8e595b00
Sridevi Tolety <9550507+sri-spirited@users.noreply.github.com>
2019-07-15 16:34:01 +0530

    Update firstNotRepeatingCharacter.py

--- firstNotRepeatingCharacter.py
+++ firstNotRepeatingCharacter.py
@@ -1,6 +1,13 @@
 def firstNotRepeatingCharacter(s):
-    lis = [a for a in s if s.count(a)==1]
-    if len(lis)==0:
-        return '_'
-    else:
-        return lis[0]
+    char_order = []
+    char_count = {}
+    for char in s:
+        if char in char_count:
+            char_count[char]+=1
+        else:
+            char_count[char]=1
+            char_order.append(char)
+    for char in char_order:
+        if char_count[char]==1:
+            return char
+    return '_'
